9439d6dafa9517006aa27580312d67616a74c8cd
Attila Bagossy <bagossyattila@outlook.com>
2021-06-09 22:04:15 +0200

    refactor: removed the retention timeout as it's not necessary

--- src/main/kotlin/hu/stepintomeetups/whoami/configuration/StartupConfiguration.kt
+++ src/main/kotlin/hu/stepintomeetups/whoami/configuration/StartupConfiguration.kt
@@ -13,13 +13,11 @@ data class StartupConfiguration(
     val botToken: String,
     val introductionChannelIdentifier: String,
     val guildIdentifier: String,
-    val introductionRetentionSeconds: Int,
 ) {
     companion object {
         const val BOT_TOKEN_VARIABLE = "WHOAMI_BOT_TOKEN"
         const val INTRODUCTION_CHANNEL_IDENTIFIER_VARIABLE = "WHOAMI_INTRODUCTION_CHANNEL_IDENTIFIER"
         const val GUILD_IDENTIFIER_VARIABLE = "WHOAMI_GUILD_IDENTIFIER"
-        const val INTRODUCTION_RETENTION_SECONDS_VARIABLE = "WHOAMI_INTRODUCTION_RETENTION_SECONDS"
 
         /**
          * Attempts to construct a new [StartupConfiguration] instance by loading
@@ -31,25 +29,9 @@ data class StartupConfiguration(
             getEnvOrThrow(BOT_TOKEN_VARIABLE),
             getEnvOrThrow(INTRODUCTION_CHANNEL_IDENTIFIER_VARIABLE),
             getEnvOrThrow(GUILD_IDENTIFIER_VARIABLE),
-            ensureGreaterThanZero(INTRODUCTION_RETENTION_SECONDS_VARIABLE, getEnvOrThrow(INTRODUCTION_RETENTION_SECONDS_VARIABLE))
         )
 
         private fun getEnvOrThrow(key: String) =
             System.getenv(key) ?: throw InvalidConfigurationException("The following environment variable is missing: $key")
-
-        private fun ensureGreaterThanZero(key: String, value: String): Int {
-            val intValue: Int
-            try {
-                intValue = value.toInt()
-            } catch (e: NumberFormatException) {
-                throw InvalidConfigurationException("$key must be an integer value!")
-            }
-
-            if (intValue < 1) {
-                throw InvalidConfigurationException("$key must be greater than 0!")
-            }
-
-            return intValue
-        }
     }
 }
